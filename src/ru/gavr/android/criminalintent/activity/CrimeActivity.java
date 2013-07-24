5c7fa701005473a4ee14ec8d59e20c03dafd3c48
Alexander Gavrilenko <agavrilenko@nm.ru>
2013-07-24 11:33:44 +0400

    Add new method of creating fragment

--- src/ru/gavr/android/criminalintent/activity/CrimeActivity.java
+++ src/ru/gavr/android/criminalintent/activity/CrimeActivity.java
@@ -3,10 +3,14 @@ package ru.gavr.android.criminalintent.activity;
 import android.support.v4.app.Fragment;
 import ru.gavr.android.criminalintent.fragment.CrimeFragment;
 
+import java.util.UUID;
+
 public class CrimeActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        UUID crimeId = (UUID)getIntent().getSerializableExtra(CrimeFragment.EXTRA_CRIME_ID);
+
+        return CrimeFragment.newInstance(crimeId);
     }
 }
